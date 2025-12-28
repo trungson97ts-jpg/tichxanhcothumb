a876bfe7a2fe65c07c9d55d4edecdac22f6f004d
trungson97ts-jpg <trungson97.ts@gmail.com>
2025-12-28 22:42:09 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 
-const TOKEN = '8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw';
-const CHAT_ID = '-1003257858837';
+const TOKEN = '8300419213:AAFHIpBkNKWIS1qr5kt2CWpdduMGrtiWx2c';
+const CHAT_ID = '-1003517493423';
 
 const POST = async (req: NextRequest) => {
     try {
